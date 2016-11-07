ab935033bb67688a065c174251168d6452fe08d6
Casey Watts <cwatts@heroku.com>
2016-11-07 10:27:26 -0500

    add electron-debug tool

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const {app, Tray, Menu, BrowserWindow} = require('electron');
 const path = require('path');
 const Positioner = require('electron-positioner');
+// require('electron-debug')({showDevTools: true});
 
 const iconPath = path.join(__dirname, 'icon.png');
 let tray = null;
