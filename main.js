0f74665009d9909fb33a5277bffdc9911da5e3be
Casey Watts <cwatts@heroku.com>
2016-11-07 15:05:08 -0500

    wip

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const {app, Tray, Menu, BrowserWindow} = require('electron');
 const path = require('path');
 const Positioner = require('electron-positioner');
-// require('electron-debug')({showDevTools: true}); // for debugging
+const socket = require('socket.io-client')('https://simi.heroku.com/pipelines/123/pipeline-couplings');
 
 const iconPath = path.join(__dirname, 'icon.png');
 let tray = null;
@@ -39,4 +39,11 @@ app.on('ready', function(){
     setTrayIcon(someData);
   }
   setTimeout(dispatchEventForSomeData, 1000, someData);
+
+
+
+socket.on('connect', function(){});
+socket.on('event', function(data){});
+socket.on('disconnect', function(){});
+  "969872eb-4e0d-42b8-87f1-cca4dad8c1f7"
 });
